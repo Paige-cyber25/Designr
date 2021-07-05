721b1c21b1ec97c555eeb1587815d47825ea8055
Paige25 <patience@kobo360.com>
2021-07-05 10:26:48 +0100

    new commits

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,22 +1,69 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Carousel} from 'react-bootstrap';
+import {instance} from '../redux/api/config';
+import YouTubePlayer from 'react-player/lib/players/YouTube';
 
 function CarouselContainer() {
+    const [courses, setCourses] = useState([]);
+    const [play, setPlay] = useState(false);
+    useEffect(()=> {
+      async function getCourses() {
+        try {
+          const result = await instance.get('/courses', {
+            headers: {
+              'x-access-token': JSON.parse(localStorage.getItem('profile')).token,
+            }
+          })
+
+          setCourses(result.data.courses);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+        getCourses();
+    },[])
+
     return (
         <div className="carousel">
-                <Carousel  className="carousel-container container text-center carousel-dark">
-                <Carousel.Item className="">
-                <img
-                className="img1"
-                src="/carousel-img1.png"
-                alt="First slide"
-                />
-                <img src="/play-button.png" alt="play button" className="play-button1" />
-                <Carousel.Caption className="caption-container">
-                <h3 className="carousel-caption-title">Typography design 101: A guide to rules and terms</h3>
-                <p className="carousel-caption-name">Zane Malik</p>
-                </Carousel.Caption>
-                <img
+                <Carousel  className="carousel-container container text-center carousel-dark" >
+                
+                {courses.map((course,index) => {
+                  return (
+                    <Carousel.Item className="" key={index} >
+                      {/* <YouTubePlayer
+                      width="364"
+                      height="284" 
+                      url={course.Link} 
+                      
+                      
+                      /> */}
+                      {/* <iframe src={course.Link}
+                      frameBorder='0'
+                      allow='autoplay; encrypted-media'
+                      allowFullScreen
+                      title='video'
+/> */}                
+                    <iframe width="420" height="345" src={course.Link} title={course.Resource_Type}>
+</iframe>
+                    
+                      <button onClick={() => setPlay(true)} >
+                        <img 
+                        src="/play-button.png" 
+                        alt="play button" 
+                        className="play-button1"
+                        onClick={() => setPlay(true)}
+                        style={{cursor:'pointer'}}/>
+                      </button>
+                      <Carousel.Caption className="">
+                      <h3 className="carousel-caption-title" style={{fontSize:'16px'}}>{course.Title}</h3>
+                      <p className="carousel-caption-name" style={{fontSize:'12px'}}>{course.Keyword}</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                  )
+                  
+                })}
+                
+                {/* <img
                     className="img2"
                     src="/carousel-img2.png"
                     alt="Second slide"
@@ -131,8 +178,8 @@ function CarouselContainer() {
                  <Carousel.Caption className="caption-container">
                 <h3 className="carousel-caption-title">Mobile app UI design: How to do It like a pro</h3>
                 <p>Faith Aliu</p>
-                </Carousel.Caption>
-                </Carousel.Item>
+                </Carousel.Caption> */}
+                
         </Carousel>
 
             </div>
